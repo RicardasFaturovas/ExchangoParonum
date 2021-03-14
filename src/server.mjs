e014ee6937bdccd7628672972d3e3824e7f850dd
Ricardas Faturovas <ricardas.faturovas@devbridge.com>
2021-03-14 18:47:49 +0200

    Fix import issues

--- src/server.mjs
+++ src/server.mjs
@@ -1,4 +1,4 @@
 import app from './index.mjs';
-import { port } from './config/serverConfig.mjs';
+import { PORT } from './config/serverConfig.mjs';
 
-app.listen(port, () => console.info(`Server running in http://127.0.0.1:${port}`));
+app.listen(PORT, () => console.info(`Server running in http://127.0.0.1:${PORT}`));
--- src/services/CurrencyExchangeService.mjs
+++ src/services/CurrencyExchangeService.mjs
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 import { BASE_CURRENCY_EXCHANGE_API_URL } from '../utilites/constants.mjs';
 
-export class CurrenyExchangeService {
+export class CurrencyExchangeService {
     async fetchCurrencies(baseCurrency, quoteCurrency) {
         try {
             return await fetch(`${BASE_CURRENCY_EXCHANGE_API_URL}?base=${baseCurrency}&symbols=${quoteCurrency}`);
